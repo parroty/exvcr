209c47923edeff37f2dc09997ab1fc9ed0f8d6f8
parroty <parroty00@gmail.com>
2013-11-19 00:54:11 +0900

    Refactor TaskRunner logics

--- lib/exvcr/task_runner.ex
+++ lib/exvcr/task_runner.ex
@@ -3,7 +3,9 @@ defmodule ExVCR.TaskRunner do
   Provides task processing logics, which will be invoked by custom mix tasks.
   """
   import ExPrintf
-  @print_format "  %-40s %-30s\n"
+  @header_format "  %-40s %-30s\n"
+  @count_format  "  %-40s %-30d\n"
+  @date_format   "%04d/%02d/%02d %02d:%02d:%02d"
   @json_file_pattern %r/\.json$/
 
   @doc """
@@ -13,6 +15,29 @@ defmodule ExVCR.TaskRunner do
     read_cassettes(path) |> print_cassettes(path)
   end
 
+  defp read_cassettes(path) do
+    file_names = find_json_files(path)
+    date_times = Enum.map(file_names, &(extract_last_modified_time(path, &1)))
+    Enum.zip(file_names, date_times)
+  end
+
+  defp find_json_files(path) do
+    File.ls!(path) |> Enum.filter(&(&1 =~ @json_file_pattern))
+                   |> Enum.sort
+  end
+
+  defp extract_last_modified_time(path, file_name) do
+    {{year, month, day}, {hour, min, sec}} = File.stat!(Path.join(path, file_name)).mtime
+    sprintf(@date_format, [year, month, day, hour, min, sec])
+  end
+
+  defp print_cassettes(items, path) do
+    IO.puts "Showing list of cassettes in [#{path}]"
+    printf(@header_format, ["[File Name]", "[Last Update]"])
+    Enum.each(items, fn({name, date}) -> printf(@header_format, [name, date]) end)
+  end
+
+
   @doc """
   Use specified path to delete cassettes.
   """
@@ -20,38 +45,6 @@ defmodule ExVCR.TaskRunner do
     path |> find_json_files
          |> Enum.filter(&(&1 =~ file_patterns))
          |> Enum.each(&(delete_and_print_name(path, &1, is_interactive)))
-  end
-
-  @doc """
-  Check and show which cassettes are used by the test execution.
-  """
-  def check_cassettes(record) do
-    count_hash = create_count_hash(record.files)
-    Enum.each(record.dirs, fn(dir) ->
-      cassettes = read_cassettes(dir)
-      print_check_cassettes(cassettes, dir, count_hash)
-      IO.puts ""
-    end)
-  end
-
-  defp create_count_hash(files) do
-    do_create_count_hash(files, HashDict.new)
-  end
-
-  defp do_create_count_hash([], acc), do: acc
-  defp do_create_count_hash([head|tail], acc) do
-    file = Path.basename(head)
-    count = HashDict.get(acc, file, 0)
-    hash  = HashDict.put(acc, file, count + 1)
-    do_create_count_hash(tail, hash)
-  end
-
-  defp print_check_cassettes(items, path, counts_hash) do
-    IO.puts "Showing hit counts of cassettes in [#{path}]"
-    printf(@print_format, ["[File Name]", "[Hit Counts]"])
-    Enum.each(items, fn({name, _date}) ->
-      printf("  %-40s %-30d\n", [name, HashDict.get(counts_hash, name, 0)])
-    end)
   end
 
   defp delete_and_print_name(path, file_name, true) do
@@ -68,25 +61,32 @@ defmodule ExVCR.TaskRunner do
     end
   end
 
-  defp read_cassettes(path) do
-    file_names = find_json_files(path)
-    date_times = Enum.map(file_names, &(extract_last_modified_time(path, &1)))
-    Enum.zip(file_names, date_times)
+
+  @doc """
+  Check and show which cassettes are used by the test execution.
+  """
+  def check_cassettes(record) do
+    count_hash = create_count_hash(record.files, HashDict.new)
+    Enum.each(record.dirs, fn(dir) ->
+      cassettes = read_cassettes(dir)
+      print_check_cassettes(cassettes, dir, count_hash)
+      IO.puts ""
+    end)
   end
 
-  defp find_json_files(path) do
-    File.ls!(path) |> Enum.filter(&(&1 =~ @json_file_pattern))
-                   |> Enum.sort
+  defp create_count_hash([], acc), do: acc
+  defp create_count_hash([head|tail], acc) do
+    file = Path.basename(head)
+    count = HashDict.get(acc, file, 0)
+    hash  = HashDict.put(acc, file, count + 1)
+    create_count_hash(tail, hash)
   end
 
-  defp extract_last_modified_time(path, file_name) do
-    {{year, month, day}, {hour, min, sec}} = File.stat!(Path.join(path, file_name)).mtime
-    sprintf("%04d/%02d/%02d %02d:%02d:%02d", [year, month, day, hour, min, sec])
-  end
-
-  defp print_cassettes(items, path) do
-    IO.puts "Showing list of cassettes in [#{path}]"
-    printf(@print_format, ["[File Name]", "[Last Update]"])
-    Enum.each(items, fn({name, date}) -> printf(@print_format, [name, date]) end)
+  defp print_check_cassettes(items, path, counts_hash) do
+    IO.puts "Showing hit counts of cassettes in [#{path}]"
+    printf(@header_format, ["[File Name]", "[Hit Counts]"])
+    Enum.each(items, fn({name, _date}) ->
+      printf(@count_format, [name, HashDict.get(counts_hash, name, 0)])
+    end)
   end
 end
