f1d5a832dd31a69f84dd88d4edd3e5cfdfdffc42
Joe-noh <goflb.jh@gmail.com>
2014-07-05 04:23:41 +0900

    Compat v0.14.2
    use on_exit/2 instead of teardown
    on_exit/2 in setup/2 runs after each test case
    on_exit/2 in setup_all/2 runs after the test suite finished

--- test/recorder_ibrowse_test.exs
+++ test/recorder_ibrowse_test.exs
@@ -7,6 +7,11 @@ defmodule ExVCR.RecorderIBrowseTest do
   @url "http://localhost:#{@port}/server"
 
   setup_all do
+    on_exit fn ->
+      File.rm_rf(@dummy_cassette_dir)
+      :ok
+    end
+
     :ibrowse.start
     HttpServer.start(path: "/server", port: @port, response: "test_response")
     ExVCR.Config.cassette_library_dir(@dummy_cassette_dir)
@@ -56,8 +61,4 @@ defmodule ExVCR.RecorderIBrowseTest do
     ExVCR.Config.filter_url_params(false)
   end
 
-  teardown_all do
-    File.rm_rf(@dummy_cassette_dir)
-    :ok
-  end
 end
