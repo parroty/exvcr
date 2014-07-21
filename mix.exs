75ef328d947c104a1e4416e93f4ee9ecae77e450
parroty <parroty00@gmail.com>
2014-07-21 23:11:32 +0900

    Fix deps in mix.exs

--- mix.exs
+++ mix.exs
@@ -3,9 +3,9 @@ defmodule ExVCR.Mixfile do
 
   def project do
     [ app: :exvcr,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 0.14.2",
-      deps: deps(Mix.env),
+      deps: deps,
       description: description,
       package: package,
       test_coverage: [tool: ExCoveralls]
@@ -19,26 +19,16 @@ defmodule ExVCR.Mixfile do
 
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
-  def deps(:test) do
-    deps(:dev)
-  end
-
-  def deps(:dev) do
-    deps(:prod) ++
-      [
-        {:ibrowse, github: "cmullaparthi/ibrowse", ref: "866b0ff5aca229f1ef53653eabc8ed1720c13cd6", override: true},
-        {:httpotion, "~> 0.2"},
-        {:httpoison, github: "edgurgel/httpoison"},
-        {:excoveralls, "~> 0.3"},
-        {:http_server, github: "parroty/http_server"}
-      ]
-  end
-
-  def deps(:prod) do
+  def deps do
     [
       {:meck, "0.8.2", github: "eproxus/meck"},
       {:exactor, "~> 0.3"},
-      {:jsex, "~> 2.0"}
+      {:jsex, "~> 2.0"},
+      {:ibrowse, github: "cmullaparthi/ibrowse", ref: "866b0ff5aca229f1ef53653eabc8ed1720c13cd6", override: true, optional: true},
+      {:httpotion, "~> 0.2", optional: true},
+      {:httpoison, github: "edgurgel/httpoison", optional: true},
+      {:excoveralls, "~> 0.3", only: :test},
+      {:http_server, github: "parroty/http_server", only: :test}
     ]
   end
 
