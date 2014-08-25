4a554cbd65a2d410ee435f22ec796288422e3eb2
parroty <parroty00@gmail.com>
2014-08-26 01:51:03 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExVCR.Mixfile do
 
   def project do
     [ app: :exvcr,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 0.15.0",
       deps: deps,
       description: description,
