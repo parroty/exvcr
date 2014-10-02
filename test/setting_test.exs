bc91a689da0f92dd7637389d24503802ce2c8607
parroty <parroty00@gmail.com>
2014-10-03 00:21:39 +0900

    Try to fix unstable test

--- test/setting_test.exs
+++ test/setting_test.exs
@@ -1,16 +1,25 @@
 defmodule ExVCR.SettingTest do
   use ExUnit.Case, async: false
 
-  test "default vcr path" do
-    assert ExVCR.Setting.get(:cassette_library_dir) == "fixture/vcr_cassettes"
+  setup_all do
+    cassette_library_dir = ExVCR.Setting.get(:cassette_library_dir)
+    custom_library_dir   = ExVCR.Setting.get(:custom_library_dir)
+
+    on_exit(fn ->
+      ExVCR.Setting.set(:cassette_library_dir, cassette_library_dir)
+      ExVCR.Setting.set(:custom_library_dir, custom_library_dir)
+      :ok
+    end)
+    :ok
   end
 
-  test "default custom path" do
-    assert ExVCR.Setting.get(:custom_library_dir) == "fixture/custom_cassettes"
+  test "set custom_library_dir" do
+    ExVCR.Setting.set(:custom_library_dir, "custom_dummy")
+    assert ExVCR.Setting.get(:custom_library_dir) == "custom_dummy"
   end
 
   test "set cassette_library_dir" do
-    ExVCR.Setting.set(:cassette_library_dir, "dummy")
-    assert ExVCR.Setting.get(:cassette_library_dir) == "dummy"
+    ExVCR.Setting.set(:cassette_library_dir, "cassette_dummy")
+    assert ExVCR.Setting.get(:cassette_library_dir) == "cassette_dummy"
   end
 end
