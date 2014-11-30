ce791d03db6a21e6fe1630a2293090874fe8af5b
parroty <parroty00@gmail.com>
2014-11-30 19:48:44 +0900

    Make it require elixir v1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExVCR.Mixfile do
   def project do
     [ app: :exvcr,
       version: "0.3.4",
-      elixir: "~> 0.15.0 or ~> 1.0.0",
+      elixir: "~> 1.0.0",
       deps: deps,
       description: description,
       package: package,
