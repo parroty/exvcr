c510fe7000b5314ec3156a3268ef5d36a04796cf
parroty <parroty00@gmail.com>
2014-11-30 20:02:05 +0900

    Make elixir requirement to v1.0 instead of v1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExVCR.Mixfile do
   def project do
     [ app: :exvcr,
       version: "0.3.4",
-      elixir: "~> 1.0.0",
+      elixir: "~> 1.0",
       deps: deps,
       description: description,
       package: package,
