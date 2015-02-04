b187d53cc35a4005312e30fbbadb751c9aeb73df
parroty <parroty00@gmail.com>
2015-02-04 20:54:38 +0900

    Consolidate [custom: true] related tests into single file

--- test/adapter_ibrowse_test.exs
+++ test/adapter_ibrowse_test.exs
@@ -78,48 +78,6 @@ defmodule ExVCR.Adapter.IBrowseTest do
     end
   end
 
-  test "custom with valid response" do
-    use_cassette "response_mocking", custom: true do
-      assert HTTPotion.get("http://example.com", []).body =~ ~r/Custom Response/
-    end
-  end
-
-  test "custom response with regexp url" do
-    use_cassette "response_mocking_regex", custom: true do
-      HTTPotion.get("http://example.com/something/abc", []).body =~ ~r/Custom Response/
-    end
-  end
-
-  test "custom without valid response throws error" do
-    assert_raise ExVCR.InvalidRequestError, fn ->
-      use_cassette "response_mocking", custom: true do
-        HTTPotion.get("http://invalidurl.example.com/invalid", [])
-      end
-    end
-  end
-
-  test "custom without valid response file throws error" do
-    assert_raise ExVCR.FileNotFoundError, fn ->
-      use_cassette "invalid_file_response", custom: true do
-        HTTPotion.get("http://example.com", [])
-      end
-    end
-  end
-
-  test "match method succeeds" do
-    use_cassette "method_mocking", custom: true do
-      HTTPotion.post("http://example.com", "").body =~ ~r/Custom Response/
-    end
-  end
-
-  test "match method fails" do
-    assert_raise ExVCR.InvalidRequestError, fn ->
-      use_cassette "method_mocking", custom: true do
-        HTTPotion.put("http://example.com", "").body =~ ~r/Custom Response/
-      end
-    end
-  end
-
   test "get fails with timeout" do
     assert_raise HTTPotion.HTTPError, fn ->
       use_cassette "httpotion_get_timeout" do
