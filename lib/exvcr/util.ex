101286f3acb2e3b11add95aa229707a54b092950
Ross Jones <ross@servercode.co.uk>
2015-05-30 21:37:42 +0100

    Replace erlang.now with os.timestamp
    :erlang.now is deprecated in Erlang 18, and so this change its use to
    os.timestamp instead.

--- lib/exvcr/util.ex
+++ lib/exvcr/util.ex
@@ -7,6 +7,6 @@ defmodule ExVCR.Util do
   Returns uniq_id string based on current timestamp (ex. 1407237617115869)
   """
   def uniq_id do
-    :erlang.now |> Tuple.to_list |> Enum.join("")
+    :os.timestamp |> Tuple.to_list |> Enum.join("")
   end
 end
