7f725c5caaf8260dce87dfd1e181372c07cf8149
Mike Boone <mike@boonedocks.net>
2016-05-23 22:04:29 -0400

    Refactor remove_blacklisted_headers test.

--- test/filter_test.exs
+++ test/filter_test.exs
@@ -1,8 +1,6 @@
 defmodule ExVCR.FilterTest do
   use ExUnit.Case, async: false
 
-  test "remove_blacklisted_headers with empty headers" do
-    assert ExVCR.Filter.remove_blacklisted_headers([]) == []
   test "filter_sensitive_data" do
     ExVCR.Config.filter_sensitive_data("<PASSWORD>.+</PASSWORD>", "PLACEHOLDER")
     ExVCR.Config.filter_sensitive_data("secret", "PLACEHOLDER")
@@ -15,7 +13,6 @@ defmodule ExVCR.FilterTest do
     ExVCR.Config.filter_sensitive_data(nil)
   end
 
-  test "remove_blacklisted_headers with supplied headers" do
   test "filter_url_params" do
     url = "https://example.com/api?test1=foo&test2=bar"
 
@@ -40,9 +37,15 @@ defmodule ExVCR.FilterTest do
     url = "https://example.com?test1=foo&test2=bar"
     assert ExVCR.Filter.strip_query_params(url) == "https://example.com"
   end
+
+  test "remove_blacklisted_headers" do
+    assert ExVCR.Filter.remove_blacklisted_headers([]) == []
+
     ExVCR.Config.response_headers_blacklist(["X-Filter1", "X-Filter2"])
     headers = [{"X-Filter1", "1"}, {"x-filter2", "2"}, {"X-NoFilter", "3"}]
     filtered_headers = ExVCR.Filter.remove_blacklisted_headers(headers)
     assert filtered_headers == [{"X-NoFilter", "3"}]
+
+    ExVCR.Config.response_headers_blacklist([])
   end
 end
