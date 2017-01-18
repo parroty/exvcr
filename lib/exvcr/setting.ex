6869a60cbdb8914678508a898b777967261097a1
Angelo Lakra <alakra@comverge.com>
2017-01-18 12:29:12 -0700

    Loading Mix.Config for "vcr_cassettes" and "custom_cassettes" for Mix.Tasks
      NOTE: Falls back to defaults if configuration is not specified.

--- lib/exvcr/setting.ex
+++ lib/exvcr/setting.ex
@@ -4,8 +4,7 @@ defmodule ExVCR.Setting do
   """
 
   @ets_table :exvcr_setting
-  @default_vcr_path    "fixture/vcr_cassettes"
-  @default_custom_path "fixture/custom_cassettes"
+
 
   def get(key) do
     setup()
@@ -20,9 +19,6 @@ defmodule ExVCR.Setting do
   def append(key, value) do
     set(key, [value | ExVCR.Setting.get(key)])
   end
-
-  def get_default_vcr_path, do: @default_vcr_path
-  def get_default_custom_path, do: @default_custom_path
 
   defp setup do
     if :ets.info(@ets_table) == :undefined do
