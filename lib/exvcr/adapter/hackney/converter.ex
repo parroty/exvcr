12269366879c10f1af7764abc589d720f6a418c7
David Whitlock <alovedalongthe@gmail.com>
2021-04-29 17:41:01 +0700

    Fix sanitize options function in hackney adapter converter (#169)

--- lib/exvcr/adapter/hackney/converter.ex
+++ lib/exvcr/adapter/hackney/converter.ex
@@ -6,12 +6,12 @@ defmodule ExVCR.Adapter.Hackney.Converter do
   use ExVCR.Converter
 
   defp string_to_response(string) do
-    response = Enum.map(string, fn({x, y}) -> {String.to_atom(x), y} end)
+    response = Enum.map(string, fn {x, y} -> {String.to_atom(x), y} end)
     response = struct(ExVCR.Response, response)
 
     response =
       if is_map(response.headers) do
-        headers = response.headers |> Map.to_list
+        headers = response.headers |> Map.to_list()
         %{response | headers: headers}
       else
         response
@@ -21,10 +21,10 @@ defmodule ExVCR.Adapter.Hackney.Converter do
   end
 
   defp request_to_string(request) do
-    method  = Enum.fetch!(request, 0) |> to_string()
-    url     = Enum.fetch!(request, 1) |> parse_url()
+    method = Enum.fetch!(request, 0) |> to_string()
+    url = Enum.fetch!(request, 1) |> parse_url()
     headers = Enum.at(request, 2, []) |> parse_headers()
-    body    = Enum.at(request, 3, "") |> parse_request_body()
+    body = Enum.at(request, 3, "") |> parse_request_body()
     options = Enum.at(request, 4, []) |> sanitize_options() |> parse_options()
 
     %ExVCR.Request{
@@ -36,26 +36,46 @@ defmodule ExVCR.Adapter.Hackney.Converter do
     }
   end
 
-  # If option value is tuple, make it as list, for encoding as json.
+  # Sanitize options so that they can be encoded as json.
   defp sanitize_options(options) do
-    Enum.map(options, fn
-      {key, value} ->
-        if is_tuple(value) do
-          {key, Tuple.to_list(value)}
-        else
-          {key, value}
-        end
-      key when is_atom(key) ->
-        {key, true}
-    end)
+    Enum.map(options, &do_sanitize/1)
+  end
+
+  defp do_sanitize({key, value}) when is_function(key) do
+    {inspect(key), value}
+  end
+
+  defp do_sanitize({key, value}) when is_list(value) do
+    {key, Enum.map(value, &do_sanitize/1)}
+  end
+
+  defp do_sanitize({key, value}) when is_tuple(value) do
+    {key, Tuple.to_list(do_sanitize(value))}
+  end
+
+  defp do_sanitize({key, value}) when is_function(value) do
+    {key, inspect(value)}
+  end
+
+  defp do_sanitize({key, value}) do
+    {key, value}
+  end
+
+  defp do_sanitize(key) when is_atom(key) do
+    {key, true}
+  end
+
+  defp do_sanitize(value) do
+    value
   end
 
   defp response_to_string({:ok, status_code, headers, body_or_client}) do
-    body = case body_or_client do
-      string when is_binary(string) -> string
-      # Client is already replaced by body through ExVCR.Adapter.Hackney adapter.
-      ref when is_reference(ref) -> inspect(ref)
-    end
+    body =
+      case body_or_client do
+        string when is_binary(string) -> string
+        # Client is already replaced by body through ExVCR.Adapter.Hackney adapter.
+        ref when is_reference(ref) -> inspect(ref)
+      end
 
     %ExVCR.Response{
       type: "ok",
@@ -84,7 +104,7 @@ defmodule ExVCR.Adapter.Hackney.Converter do
     :hackney_request.encode_form(body)
     |> elem(2)
     |> to_string
-    |> ExVCR.Filter.filter_sensitive_data
+    |> ExVCR.Filter.filter_sensitive_data()
   end
 
   def parse_request_body(body), do: super(body)
