d1c17651682c7457f5393c901b62f170240d73af
Patrick Berkeley <patrickberkeley@gmail.com>
2023-08-27 06:45:17 +0200

    Normalizes request body and URL by parsing params to a list and sorting (#211)
    * Normalizes request body by parsing as params, then converting to a list
    
    Before this change, if the request body was a list of params that were
    in a different order than the params in the cassette, the request body
    match would fail.
    
    As of OTP 26, map key order is not guaranteed, so request bodies that
    are created using maps can fail to match since the order of their keys
    is not idempotent.
    
    These changes convert the request body to a list of params and sort it
    before comparing it to the request body in the cassette. This ensures
    cassettes will be matched as long as their request bodies contain the
    same set of key-value pairs as the incoming request body.
    
    * Normalizes url by parsing params, converting to a list, and sorting
    
    Before this change, if the url query params were in a different order
    than the url params in the cassette, the request body match would fail.
    
    As of OTP 26, map key order is not guaranteed, so url params that are
    created using maps can fail to match since the order of their keys is
    not idempotent.
    
    These changes convert the url params to a list and sort it before
    comparing it to the url in the cassette. This ensures cassettes will be
    matched as long as their url params contain the same set of key-value
    pairs as the incoming url params (and the rest of the url matches too).

--- test/handler_stub_mode_test.exs
+++ test/handler_stub_mode_test.exs
@@ -31,9 +31,25 @@ defmodule ExVCR.Adapter.HandlerStubModeTest do
     end
   end
 
+  test "url matches as regardless of query param order" do
+    use_cassette :stub, [url: "http://localhost?param1=10&param2=20&param3=30"] do
+      {:ok, status_code, _headers, body} = :ibrowse.send_req('http://localhost?param3=30&param1=10&param2=20', [], :get)
+      assert status_code == '200'
+      assert to_string(body) =~ ~r/Hello World/
+    end
+  end
+
   test "url matches as regex" do
     use_cassette :stub, [url: "~r/.+/"] do
       {:ok, status_code, _headers, body} = :ibrowse.send_req('http://localhost', [], :get)
+      assert status_code == '200'
+      assert to_string(body) =~ ~r/Hello World/
+    end
+  end
+
+  test "request_body matches as string" do
+    use_cassette :stub, [url: 'http://localhost', method: :post, request_body: "some-string", body: "Hello World"] do
+      {:ok, status_code, _headers, body} = :ibrowse.send_req('http://localhost', [], :post, 'some-string')
       assert status_code == '200'
       assert to_string(body) =~ ~r/Hello World/
     end
@@ -55,6 +71,22 @@ defmodule ExVCR.Adapter.HandlerStubModeTest do
     end
   end
 
+  test "request_body matches as unordered list of params" do
+    use_cassette :stub, [url: 'http://localhost', method: :post, request_body: "param1=10&param3=30&param2=20", body: "Hello World"] do
+      {:ok, status_code, _headers, body} = :ibrowse.send_req('http://localhost', [], :post, 'param2=20&param1=10&param3=30')
+      assert status_code == '200'
+      assert to_string(body) =~ ~r/Hello World/
+    end
+  end
+
+  test "request_body mismatches as unordered list of params" do
+    assert_raise ExVCR.InvalidRequestError, fn ->
+      use_cassette :stub, [url: 'http://localhost', method: :post, request_body: "param1=10&param3=30&param4=40", body: "Hello World"] do
+        {:ok, _status_code, _headers, _body} = :ibrowse.send_req('http://localhost', [], :post, 'param2=20&param1=10&param3=30')
+      end
+    end
+  end
+  
   test "request_body mismatch should raise error" do
     assert_raise ExVCR.InvalidRequestError, fn ->
       use_cassette :stub, [url: 'http://localhost', method: :post, request_body: '{"one" => 1}'] do
