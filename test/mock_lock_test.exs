b0af7b152ece569d2dfbff96bdc8dbd85a021393
Daniel Kukula <daniel.kuku@gmail.com>
2025-02-15 07:42:12 +0100

    fix tests

--- test/mock_lock_test.exs
+++ test/mock_lock_test.exs
@@ -7,21 +7,24 @@ defmodule ExVCR.MockLockTest do
     other_caller_pid = "fake_pid"
     state = %{lock_holder: other_caller_pid}
 
-    {:noreply, _new_state, _timeout} =
+    {:noreply, _new_state} =
       ExVCR.MockLock.handle_info({:do_request_lock, caller_pid, test_pid}, state)
 
     assert_receive {:do_request_lock, ^caller_pid, ^test_pid}
 
     state2 = %{lock_holder: nil}
-    {:noreply, new_state2, _timeout} =
+
+    {:noreply, new_state2} =
       ExVCR.MockLock.handle_info({:do_request_lock, caller_pid, test_pid}, state2)
+
     assert new_state2 == %{lock_holder: caller_pid}
   end
 
   test "removes lock when calling process goes down" do
     pid = "fake_pid"
     state = %{lock_holder: pid}
-    {:noreply, new_state, _timeout} =
+
+    {:noreply, new_state} =
       ExVCR.MockLock.handle_info({:DOWN, "ref", :process, pid, "reason"}, state)
 
     assert new_state == %{lock_holder: nil}
